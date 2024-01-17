54ece3ab1884ddd31588b7c240369de4008fe0bb
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-01-17 12:21:07 +0100

    Create Project

---
+++ TouchIDSwiftUI/README.md
@@ -0,0 +1,5 @@
+# TouchIDSwiftUI
+Touch ID concept animation..
+
+### Output 
+![Touch ID](touchidoutput.png)
---
+++ TouchIDSwiftUI/TouchIDSwiftUI/TouchIDSwiftUI/TouchIDSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchIDSwiftUIApp.swift
+//  TouchIDSwiftUI
+//
+//  Created by Mauro Marzocca on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchIDSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
