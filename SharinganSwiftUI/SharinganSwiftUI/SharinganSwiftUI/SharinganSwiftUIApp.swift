7352f390b2f583a8294c6626393e36975b4b382c
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-01-17 12:29:57 +0100

    Upload Sharingan Project

---
+++ SharinganSwiftUI/SharinganSwiftUI/SharinganSwiftUI/SharinganSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharinganSwiftUIApp.swift
+//  SharinganSwiftUI
+//
+//  Created by Mauro Marzocca on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SharinganSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
