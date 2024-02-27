41a666ef61abc366f7023a88a72e4b2d999b68bd
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-02-27 10:58:21 +0100

    Add QRView

---
+++ QRViewSwiftUI/QRViewSwiftUI/QRViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRViewSwiftUIApp.swift
+//  QRViewSwiftUI
+//
+//  Created by MauroMarzocca on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct QRViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
