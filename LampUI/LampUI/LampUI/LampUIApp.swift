5f3ac1b7b9087f565a44b937b58f9ee535ac5e85
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-11-10 20:09:43 +0100

    Add LampUI

---
+++ LampUI/LampUI/LampUI/LampUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampUIApp.swift
+//  LampUI
+//
+//  Created by Mauro Marzocca
+//
+
+import SwiftUI
+
+@main
+struct LampUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LampUI/README.md
@@ -0,0 +1,13 @@
+# LampUISwiftUI
+
+---
+
+Lamp UI - Animation
+
+---
+
+![On](1.png)
+
+---
+
+![Off](2.png)
